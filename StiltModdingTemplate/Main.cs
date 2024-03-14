6e5b1efac7220eb43861525a9cab8fdffb972fde
MrBanana <logande4610@gmail.com>
2024-03-14 10:32:59 -0500

    Add project files.

---
+++ StiltModdingTemplate/Main.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using StiltModdingAPI.Behaviours.Events;
+using UnityEngine;
+
+//Change the namespace to whatever you want
+namespace StiltModdingTemplate {
+    //This class is just like a normal unity script, everything is already set up for you so this class will get loaded after the api gets loaded.
+    public class Main : MonoBehaviour {
+        //Gets called when the script loads
+        void Awake() {
+            StiltEventHandler.instance.OnSceneLoaded += OnStiltSceneLoaded;
+        }
+        
+        //This will run when a new scene in stilt loads, use this instead of the unity changed scene event.
+        void OnStiltSceneLoaded(bool ContainsPlayerPrefab, bool IsMultiplayerScene) {
+            MelonLogger.Msg("scene");
+            if (ContainsPlayerPrefab) {
+                MelonLogger.Msg("player prefab scene");
+                //Run code here if you want your mod to only do stuff if the rig is in the scene
+            }
+
+            if(IsMultiplayerScene) {
+                MelonLogger.Msg("multiplayer scene");
+                //Run code here if you want your mod to only do stuff if the scene is a multiplayer scene
+            }
+        }
+
+        //Gets called every frame the mod is active
+        void Update() {
+
+        }
+    }
+}
---
+++ StiltModdingTemplate/ModLoader.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using StiltModdingAPI;
+using StiltModdingAPI.Behaviours.Events;
+
+[assembly: MelonInfo(typeof(StiltModdingTemplate.ModLoader), "modname", "1.0.0", "modauthor")]
+[assembly: MelonGame("Rekt Games", "Stilt")]
+
+//Change the namespace to whatever you want
+namespace StiltModdingTemplate {
+    public class ModLoader : MelonMod {
+        public override void OnLateInitializeMelon() {
+            base.OnLateInitializeMelon();
+            StiltEventHandler.instance.OnAPILoaded += StiltAPILoaded;
+        }
+
+        /// <summary>
+        /// This method runs when the StiltModdingAPI successfully loads
+        /// </summary>
+        void StiltAPILoaded() {
+            bool SuccessfullyLoadedScript = Utils.RegisterStiltScript<Main>();
+            //The bool turns true if the script successfully loads and false if not.
+        }
+    }
+}
